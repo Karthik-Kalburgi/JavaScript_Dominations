5487ce2be5febb47bf50151662c824ee53ad4663
Karthik-Kalburgi <kalburgikarthik0105@gmail.com>
2025-07-28 23:20:43 +0530

    Merge Sort problem

--- JavaScript_Dominations-main/JS_DSA_Sheryians_Coding_School/Arrays/MergeSort.js
+++ JavaScript_Dominations-main/JS_DSA_Sheryians_Coding_School/Arrays/MergeSort.js
@@ -1,3 +1,45 @@
 //Merge SOrt 
 //THey give two different sorted arrays  arrays we need to combine and sort them.
 
+
+
+let arr1 = [2,5,6,8,10,12,14,16,18,20];
+let arr2 = [1,3,4,8];
+// let merge = new Array(arr1.length + arr2.length);
+// console.log(merge)
+// let i=0;j=0;k=0;
+// while(i<arr1.length && j<arr2.length){
+//   if(arr1[i] < arr2[j]){
+//     merge[k++] = arr1[i++]
+//   }else{
+//     merge[k++] = arr2[j++]
+//   }
+// }
+// while(i<arr1.length){
+//   merge[k++] = arr1[i++];
+// }
+// while(j<arr2.length){
+//   merge[k++] = arr2[j++];
+// }
+
+// console.log(merge)
+
+
+let merge = new Array (arr1.length + arr2.length);
+let i=0;j=0;k=0;
+while(i<arr1.length && j<arr2.length){
+  if(arr1[i] <arr2[j]){
+    merge[k++] = arr1[i++];
+  }else{
+    merge[k++] = arr2[j++];
+  }
+}
+
+while(i<arr1.length){
+  merge[k++] = arr1[i++];
+}
+while(j<arr2.length){
+  merge[k++] = arr2[j++];
+}
+
+console.log(merge);
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
+git copy from git 
+go to a required folder open in vs code
+git clone and paste the link
+copy all the folder and dump into new folder
+open vs code do git add .
+
+
 # JavaScript_Dominations
