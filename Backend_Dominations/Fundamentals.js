012bd4433e29d4d2b6a68e15857cc1807208dd1b
Karthik-Kalburgi <kalburgikarthik0105@gmail.com>
2025-07-30 20:33:38 +0530

    Started with Backend

---
+++ Backend_Dominations/Fundamentals.js
@@ -0,0 +1,29 @@
+// let arr = [1,2,3,4,5];
+// arr.forEach(function(val){
+//   console.log(val + " Hello");
+// })
+//Map is used to create a new array by applying a function to each element of the original array.
+// It does not modify the original array but returns a new one.
+// let arr = [1,2,3,4,5];
+// let ams = arr.map(function(val){
+//   return 13;
+// })
+// console.log(ams);
+// console.log(arr);
+
+// The map function iterates over each element of the array and applies the function to it, returning a new array with the results.
+// The original array remains unchanged.
+
+
+
+// The filter() method creates a new array filled with elements that pass a test provided by a function.
+
+// The filter() method does not execute the function for empty elements.
+
+// The filter() method does not change the original array.
+
+// let arr = [1,2,3,4,5];
+// let ans  = arr.filter(function(vla){
+//   return vla >3
+// })
+// console.log(ans);
---
+++ Backend_Dominations/Readme.md
@@ -0,0 +1,9 @@
+//Backend Dominations
+
+Map when you peform map on an 4 digit array when you write inside 13 what it will do is it will ccreate 4 empty spaces coz there is 4 elements and every time it will go each array elemet what ever we wrote in return statement as 13 all values will be written 13 
+
+map() creates a new array from calling a function for every array element.
+
+map() does not execute the function for empty elements.
+
+map() does not change the original array.
