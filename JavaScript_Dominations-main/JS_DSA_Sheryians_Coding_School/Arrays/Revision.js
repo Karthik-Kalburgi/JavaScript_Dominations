32f5526c91aa0d0f39a5628d4621ba7829ed20cc
Karthik-Kalburgi <kalburgikarthik0105@gmail.com>
2025-08-14 03:15:35 +0530

    Array

--- JavaScript_Dominations-main/JS_DSA_Sheryians_Coding_School/Arrays/Revision.js
+++ JavaScript_Dominations-main/JS_DSA_Sheryians_Coding_School/Arrays/Revision.js
@@ -24,11 +24,43 @@
 
 // console.log(max);
 
-let arr = [10,3,4,12,95,85,36];
-let maxOne = arr[0];
-let maxTwo = arr[1];
-for(let i=2;i<arr.length;i++){
-  if(maxOne > maxTwo){
-    return maxOne;
+// let arr = [10,3,4,12,95,85,36];
+// let maxOne = arr[0];
+// let maxTwo = arr[1];
+// for(let i=2;i<arr.length;i++){
+//   if(maxOne > maxTwo){
+//     return maxOne;
+//   }
+// }
+
+
+
+//Reverse an Array 
+//lets take i & j when i!= j swap and increment
+
+// let arr = [10,20,30,40,50];
+// let i= 0 ; j=arr.length-1;
+// while(i !== j){
+//   let temp = arr[i];
+//   arr[i]= arr[j];
+//   arr[j] = temp;
+//   i++;
+//   j--;
+// }
+
+// console.log(arr);
+
+//MOVES ONES AND ZEROS 
+//Taking two pointer alogo starts from arr[0] both i and j
+let arr = [1,0,0,1,1,0,1,0,1,0,1,1,0,1,0,1,1,1,0,,0,0,1,0,0,1,0,1];
+let i=0; j=0;
+while(i<arr.length){
+  if(arr[i] === 0){
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    j++;
   }
+  i++;//it increments every time 
 }
+console.log(arr);
